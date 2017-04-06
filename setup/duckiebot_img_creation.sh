3062ccd59c43bad5257106256fe4447657fb2aa1
Ihar Voitka <ihvo@outlook.com>
2017-04-06 07:21:42 -0700

    Updates for RPi3 and Xenial

--- setup/duckiebot_img_creation.sh
+++ setup/duckiebot_img_creation.sh
@@ -21,11 +21,12 @@ sudo apt-get install -y vim htop byobu libav-tools curl
 # === Install ROS following http://wiki.ros.org/indigo/Installation/UbuntuARM ===
 # Setup locale, deb, and keys
 sudo update-locale LANG=C LANGUAGE=C LC_ALL=C LC_MESSAGES=POSIX
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
-wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
 # Install ROS
 sudo apt-get update
-sudo apt-get install ros-indigo-ros-base -y
+sudo apt-get install ros-kinetic-ros-base -y
 # Update rosdep
 sudo apt-get install python-rosdep
 sudo rosdep init
@@ -33,18 +34,18 @@ rosdep update
 
 # === Bus and i2c ===
 sudo apt-get install -y i2c-tools python-smbus
-sudo usermod -a -G i2c ubuntu
+sudo usermod -a -G i2c duckiebot
 # Trigger the rule (reboot will do this too)
 sudo udevadm trigger 
 
 # === PiCamera Stuff === #
 sudo apt-get install --reinstall libraspberrypi0 libraspberrypi-{bin,dev,doc}
 # Append lines to /boot/config.txt to enable the PiCamera
-printf '#Enable PiCamera Interface\n %s\n %s\n' 'start_x=1' 'gpu_mem=256' | sudo tee -a /boot/config.txt
+# printf '#Enable PiCamera Interface\n %s\n %s\n' 'start_x=1' 'gpu_mem=256' | sudo tee -a /boot/config.txt
 # Create custom rule to enable camera
-printf 'SUBSYSTEM=="vchiq",GROUP="video",MODE="0660"' | sudo tee /etc/udev/rules.d/10-vchiq-permissions.rules
+# printf 'SUBSYSTEM=="vchiq",GROUP="video",MODE="0660"' | sudo tee /etc/udev/rules.d/10-vchiq-permissions.rules
 # Add user to the video group
-sudo usermod -a -G video ubuntu
+sudo usermod -a -G video duckiebot
 
 # Install python picamera drivers
 sudo pip install picamera
@@ -55,19 +56,19 @@ sudo apt-get install gfortran -y
 
 # ==== Wireless settings ==== #
 # Install firmware and tools
-sudo apt-get install wireless-tools wpasupplicant linux-firmware
+# sudo apt-get install wireless-tools wpasupplicant linux-firmware
 # Setup /etc/network/interfaces
-printf 'allow-hotplug wlan0\n auto wlan0\n iface wlan0 inet manual\n wpa-roam /etc/wpa_supplicant/wpa_supplicant.conf\n iface default inet dhcp' | sudo tee -a /etc/network/interfaces
+# printf 'allow-hotplug wlan0\n auto wlan0\n iface wlan0 inet manual\n wpa-roam /etc/wpa_supplicant/wpa_supplicant.conf\n iface default inet dhcp' | sudo tee -a /etc/network/interfaces
 # Setup /etc/wpa_supplicat.conf header
-printf 'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n update_config=1' | sudo tee -a /etc/wpa_supplicant/wpa_supplicant.conf
+# printf 'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n update_config=1' | sudo tee -a /etc/wpa_supplicant/wpa_supplicant.conf
 # Setup duckietown network SSID
-printf 'network={\n ssid="duckietown"\n scan_ssid=1\n psk="quackquack"\n priority=10\n}' | sudo tee -a /etc/wpa_supplicant/wpa_supplicant.conf
+# printf 'network={\n ssid="duckietown"\n scan_ssid=1\n psk="quackquack"\n priority=10\n}' | sudo tee -a /etc/wpa_supplicant/wpa_supplicant.conf
 
 # Setup authorized keys for ssh access for duckietown
-cd ~
-mkdir .ssh
-chmod g-rwx,o-rwx .ssh 
-wget -O .ssh/authorized_keys https://www.dropbox.com/s/pxyou3qy1p8m4d0/duckietown_key1.pub?dl=1
+# cd ~
+# mkdir .ssh
+# chmod g-rwx,o-rwx .ssh 
+# wget -O .ssh/authorized_keys https://www.dropbox.com/s/pxyou3qy1p8m4d0/duckietown_key1.pub?dl=1
 
 #Add passwordless sudo for user ubuntu:
 # $ sudo visudo
@@ -77,7 +78,7 @@ wget -O .ssh/authorized_keys https://www.dropbox.com/s/pxyou3qy1p8m4d0/duckietow
 # Select option (1) for byobu.
 
 # Install additional ROS pkgs to apt-get
-sudo apt-get install ros-indigo-{tf-conversions,cv-bridge,image-transport,camera-info-manager,theora-image-transport}
+sudo apt-get install ros-kinetic-{tf-conversions,cv-bridge,image-transport,camera-info-manager,theora-image-transport}
 
 # List of additional system pkgs
 sudo apt-get install libyaml-cpp-dev
